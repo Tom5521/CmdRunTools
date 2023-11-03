fa56998c289623f0a9f56ef0cbe390b8b40f1380
Tom5521 <mismarttv321@gmail.com>
2023-11-03 17:20:19 -0300

    Improve windows tests

--- windows_test.go
+++ windows_test.go
@@ -4,18 +4,26 @@
 package command
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/Tom5521/CmdRunTools/command"
 )
 
+func WriteToLog(data ...any) {
+	Strdata := fmt.Sprint(data...)
+	os.WriteFile("out.log", []byte(Strdata), os.ModePerm)
+}
+
 func Test_PS(t *testing.T) {
 	cmd := command.InitCmd("ls")
-	cmd.RunWithPS(false)
+	cmd.RunWithPS(true)
 	err := cmd.Run()
+	t.Log(cmd.GetExec().String())
+	WriteToLog(cmd.GetExec())
 	if err != nil {
-		t.Log(cmd.GetExec())
-		t.Log(err.Error())
+		WriteToLog(cmd.GetExec(), "\n", err)
 		t.Fail()
 	}
 }
